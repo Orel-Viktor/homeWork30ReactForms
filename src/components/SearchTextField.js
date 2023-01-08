cda53c4588bbea1886ca6554425c200b31c9adc4
Orel-Viktor <OrelViktor93@gmail.com>
2023-01-08 17:41:03 +0300

    fix

--- src/components/SearchTextField.js
+++ src/components/SearchTextField.js
@@ -1,12 +1,13 @@
 import TextField from "@mui/material/TextField";
 
 export default function SearchTextField(props) {
-  const { name, id, label, type, variant, input } = props;
+  const { id, label, type, variant, input } = props;
   return (
     <TextField
-      value={input.value}
-      name={input.name}
-      onChange={input.onChange}
+    {...input}
+    //   value={input.value}
+    //   name={input.name}
+    //   onChange={input.onChange}
       id={id}
       label={label}
       type={type}
