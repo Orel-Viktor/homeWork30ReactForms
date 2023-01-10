5a5bf61327c2fbcb4042f9f554228857466133bf
Orel-Viktor <OrelViktor93@gmail.com>
2023-01-10 14:13:50 +0200

    homework30

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { Form, Field } from "react-final-form";
-import React, { useState, useRef, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import SearchIcon from "@mui/icons-material/Search";
-import  SearchTextField  from "./components/SearchTextField";
+import SearchTextField from "./components/SearchTextField";
 
 function App() {
-  const [inputValue, setValue] = useState("Начните поиск");
+  const onValidate = (value) => {
+    const gg = {};
+    if (value.searchInput === undefined) {
+      gg.searchInput = 'Введите значние'
+    }
+    return gg;
+
+  };
+
   const onSubmit = (formValue) => {
     console.log(formValue);
   };
@@ -15,20 +22,19 @@ function App() {
   return (
     <Form
       onSubmit={onSubmit}
+      validate={onValidate}
       render={({ handleSubmit }) => {
         return (
           <Paper
             component="form"
             onSubmit={handleSubmit}
             sx={{ width: "25%", margin: "0 auto", padding: "2px" }}
-            noValidate
             autoComplete="off"
           >
-            <Box sx={{ display: "flex", alignItems: "flex-end", mb: "20px" }}>
+            <Box component='div' sx={{ display: "flex", alignItems: "flex-end", mb: "20px" }}>
               <Field
                 component={SearchTextField}
                 name="searchInput"
-                error
                 id="standard-search"
                 label="Search field"
                 type="search"
@@ -36,7 +42,6 @@ function App() {
               />
               <SearchIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
             </Box>
-
             <Button type="submit" variant="contained">
               Find
             </Button>
--- src/components/SearchTextField.js
+++ src/components/SearchTextField.js
@@ -1,17 +1,20 @@
 import TextField from "@mui/material/TextField";
 
 export default function SearchTextField(props) {
-  const { id, label, type, variant, input } = props;
+  const { id, label, type, variant, input, meta } = props;
+  console.log(meta)
   return (
     <TextField
-    {...input}
-    //   value={input.value}
-    //   name={input.name}
-    //   onChange={input.onChange}
+      {...input}
+      // value={input.value}
+      // name={input.name}
+      // onChange={input.onChange}
+      error={Boolean(meta.error && meta.touched)}
       id={id}
       label={label}
       type={type}
       variant={variant}
+      helperText={meta.error && meta.touched ? meta.error : null}
     />
   );
 }
